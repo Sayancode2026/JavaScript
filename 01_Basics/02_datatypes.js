a56a7622bf4796296ad3b915eba99a4706d8ec10
Sayancode2026 <sayanbardhan2002@gmail.com>
2026-09-07 00:31:08 +0530

    Basics datatypes in JS

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,20 @@
+let name="Sayan Bardhan"
+let age=22
+let  isLoggedIn=true
+let number;//undefined
+
+/* 
+Int 
+BigInt
+String
+Bolean
+NULL=>Standalone value//object
+undefined  //undefined(type)
+symbol->check uniqeness
+
+object 
+
+
+*/
+
+console.log(typeof undefined);
