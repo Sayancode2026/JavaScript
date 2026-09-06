2e4a51c9d8627d72a33c7c65a74c4d0534e15f37
Sayancode2026 <sayanbardhan2002@gmail.com>
2026-09-07 00:56:34 +0530

    Type conversion in Datatypes

---
+++ 01_Basics/03_Typeconversion_datatype.js
@@ -0,0 +1,44 @@
+// let score="55abd";
+// console.log(typeof(score));
+// let valueInNumber=Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);//NaN=>Not a Number
+
+
+// let score=null;
+// console.log(typeof(score));//object
+// let valueInNumber=Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);//0
+
+
+
+// let score=undefined;
+// console.log(typeof(score));//undefined
+// let valueInNumber=Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);//NaN
+
+
+
+// let score=true;
+// console.log(typeof(score));//boolean
+// let valueInNumber=Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);//1
+
+
+// let isLoggedIn=""
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);//false
+//  1->true 0->false
+//  "Sayan"->true  ""->false  " "->true
+
+
+
+ let someNumber=35
+ let stringNumber=String(someNumber);
+ console.log(stringNumber);
+ console.log(typeof(stringNumber));
+
+
