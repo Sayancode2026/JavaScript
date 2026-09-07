8c67d08b60e86ab41448df2912f4ca9c26b0132a
Sayancode2026 <sayanbardhan2002@gmail.com>
2026-09-08 01:05:24 +0530

    Added number and Maths operations

---
+++ 01_Basics/08_number_maths.js
@@ -0,0 +1,36 @@
+// let num=400
+// console.log(num)
+
+// let num1=new Number(1000)
+// console.log(num1)
+
+// console.log(num1.toString().length)
+// console.log(num1.toFixed(2));
+
+// const num3=150.8965
+// console.log(num3.toPrecision(3))//151 it prioratize the numbers before decimel
+
+
+// let num4=1000000
+// console.log(num4.toLocaleString('en-IN'))
+
+
+ //**************************Maths************************** */
+// console.log(Math)//object
+// console.log(Math.abs(-4))//4
+// console.log(Math.round(5.4))//5
+// console.log(Math.round(5.6))//6
+// console.log(Math.ceil(4.1))//5
+// console.log(Math.floor(4.9))//4
+// console.log(Math.max(5,6,1,10,2,18))
+// console.log(Math.min(5,6,1,10,2,18))
+
+
+// console.log(Math.random())// b/w 0 to 1
+// console.log(Math.floor(Math.random()*10)+1)  //b/w 1 to 9
+
+const min=10
+const max=20
+//standard formula
+console.log(Math.floor(Math.random()*(max-min+1))+min)//b/w 10  to 20
+
