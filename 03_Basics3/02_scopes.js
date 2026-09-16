bb0be230f0d7dd8f5e4313635843700428440f45
Sayancode2026 <sayanbardhan2002@gmail.com>
2026-09-16 21:34:25 +0530

    Scope is JS updated

--- 03_Basics3/02_scopes.js
+++ 03_Basics3/02_scopes.js
@@ -11,4 +11,47 @@ console.log(a);//300
 // console.log(b);
 console.log(c);//outside the scope still printed beacuse of using var this creates bugs so we dont use var
 
+function one(){
+    const username="sayan"
 
+    function two(){
+        const website="youtube"
+        console.log(username);
+        
+    }
+    //console.log(website);//outside the scope call occured error so in nested function the child func can access the parent function variable but nt vice-versa
+    two()
+    
+}
+one()
+
+
+
+if(true){
+    const username="Sayan"
+    if(username==="Sayan"){
+        const website=" youtube"
+        console.log(username+website);
+        
+    }
+    //console.log(website);//gave error accessing outside the inner func scope
+    
+}
+//console.log(username)//gave error accessing outside the outer func scope
+
+
+//++++++++++++++++++++++++++Concept++++++++++++++++++++++++
+
+console.log(addone(6));
+function addone(num){
+    return num+1
+}
+console.log(addone(7));
+
+
+
+//console.log(addTwo(2));//Cannot access 'addTwo' before initialization if assigned in variable 
+const addTwo=function(num){
+    return num+2
+}
+console.log(addTwo(2));//works well here 
